3a0a5b801286b1c8e98c24d394b689a801304b77
austin-schoster7 <schostaj@mail.uc.edu>
2025-03-27 16:58:01 -0400

    Added axis labels and titles to the graphs

--- js/magnitudeChart.js
+++ js/magnitudeChart.js
@@ -2,71 +2,106 @@ class MagnitudeChart {
     constructor(_config, _data) {
       this.config = {
         parentElement: _config.parentElement,
-        margin: { top: 20, right: 20, bottom: 30, left: 40 },
-        width: 500,
-        height: 300,
+        margin: { top: 40, right: 30, bottom: 50, left: 60 },
         bins: 10,
+        // We'll ignore width/height here, or keep them for reference.
+        // We'll define an internal coordinate system below.
         ..._config
       };
       this.data = _data;
-      // default attribute is 'mag'
       this.currentAttribute = 'mag';
+  
+      // Define an internal coordinate system
+      this.internalWidth = 600;
+      this.internalHeight = 400;
+  
       this.initVis();
     }
   
     initVis() {
       const vis = this;
-      vis.width = vis.config.width - vis.config.margin.left - vis.config.margin.right;
-      vis.height = vis.config.height - vis.config.margin.top - vis.config.margin.bottom;
   
-      vis.svg = d3.select(vis.config.parentElement).append('svg')
-        .attr('width', vis.config.width)
-        .attr('height', vis.config.height)
-        .append('g')
+      // Create a responsive SVG with a fixed "internal" coordinate system
+      vis.svg = d3.select(vis.config.parentElement)
+        .append('svg')
+        .attr('viewBox', `0 0 ${vis.internalWidth} ${vis.internalHeight}`)
+        .attr('preserveAspectRatio', 'xMidYMid meet')
+        .classed('svg-content', true);
+  
+      // Now define the actual width/height for the chart area minus margins
+      vis.width = vis.internalWidth - vis.config.margin.left - vis.config.margin.right;
+      vis.height = vis.internalHeight - vis.config.margin.top - vis.config.margin.bottom;
+  
+      // Append a <g> for the "chart area" that accounts for margins
+      vis.chartArea = vis.svg.append('g')
         .attr('transform', `translate(${vis.config.margin.left}, ${vis.config.margin.top})`);
   
-      // xScale based on current attribute
-      const attrExtent = d3.extent(vis.data, d => d[vis.currentAttribute]);
-      vis.xScale = d3.scaleLinear()
-        .domain(attrExtent)
-        .range([0, vis.width])
-        .nice();
-  
-      // create histogram
-      vis.histogram = d3.histogram()
-        .value(d => d[vis.currentAttribute])
-        .domain(vis.xScale.domain())
-        .thresholds(vis.xScale.ticks(vis.config.bins));
-  
+      // Create scales
+      vis.xScale = d3.scaleLinear().range([0, vis.width]);
       vis.yScale = d3.scaleLinear().range([vis.height, 0]);
+  
+      // Create histogram generator
+      vis.histogram = d3.histogram().thresholds(10); // We’ll set domain/value in updateVis
   
       // Axes
       vis.xAxis = d3.axisBottom(vis.xScale).ticks(5);
       vis.yAxis = d3.axisLeft(vis.yScale).ticks(5);
   
-      vis.xAxisGroup = vis.svg.append('g')
+      vis.xAxisGroup = vis.chartArea.append('g')
         .attr('class', 'x-axis')
         .attr('transform', `translate(0, ${vis.height})`);
   
-      vis.yAxisGroup = vis.svg.append('g').attr('class', 'y-axis');
+      vis.yAxisGroup = vis.chartArea.append('g')
+        .attr('class', 'y-axis');
+  
+      // X-axis label
+      vis.xLabel = vis.chartArea.append('text')
+        .attr('class', 'axis-label')
+        .attr('x', vis.width / 2)
+        .attr('y', vis.height + 35) // below x-axis
+        .style('text-anchor', 'middle')
+        .text('Magnitude');
+  
+      // Y-axis label
+      vis.yLabel = vis.chartArea.append('text')
+        .attr('class', 'axis-label')
+        .attr('transform', 'rotate(-90)')
+        .attr('x', -vis.height / 2)
+        .attr('y', -35) // push it out from the axis
+        .style('text-anchor', 'middle')
+        .text('Count');
+  
+      // Title
+      vis.title = vis.chartArea.append('text')
+        .attr('class', 'chart-title')
+        .attr('x', vis.width / 2)
+        .attr('y', -10)
+        .style('text-anchor', 'middle')
+        .style('font-size', '16px')
+        .text('Histogram of Earthquakes by Magnitude');
   
       vis.updateVis();
     }
   
     updateVis() {
       const vis = this;
+  
+      // 1) Update xScale domain based on currentAttribute
+      const attrExtent = d3.extent(vis.data, d => d[vis.currentAttribute]);
+      vis.xScale.domain(attrExtent).nice();
+  
+      // 2) Create bins
+      vis.histogram
+        .value(d => d[vis.currentAttribute])
+        .domain(vis.xScale.domain());
+  
       const bins = vis.histogram(vis.data);
+  
+      // 3) Update yScale domain
       vis.yScale.domain([0, d3.max(bins, d => d.length)]);
   
-      // We'll create a simple lookup for attribute labels
-      const attributeLabels = {
-        mag: 'Magnitude',
-        depth: 'Depth',
-      };
-      const currentLabel = attributeLabels[vis.currentAttribute] || 'Value';
-  
-      // draw rects
-      vis.bars = vis.svg.selectAll('.bar')
+      // 4) Draw bars
+      vis.bars = vis.chartArea.selectAll('.bar')
         .data(bins)
         .join('rect')
           .attr('class', 'bar')
@@ -75,12 +110,12 @@ class MagnitudeChart {
           .attr('y', d => vis.yScale(d.length))
           .attr('height', d => vis.height - vis.yScale(d.length))
           .attr('fill', 'teal')
-          // Tooltip handlers:
+          // Tooltip
           .on('mouseover', (event, d) => {
             d3.select('#tooltip')
               .style('opacity', 1)
               .html(`
-                <b>${currentLabel} range:</b> ${d.x0.toFixed(2)} - ${d.x1.toFixed(2)}<br>
+                <b>Range:</b> ${d.x0.toFixed(2)} - ${d.x1.toFixed(2)}<br>
                 <b>Count:</b> ${d.length}
               `);
           })
@@ -94,7 +129,7 @@ class MagnitudeChart {
               .style('opacity', 0);
           });
   
-      // Update axes
+      // 5) Update axes
       vis.xAxisGroup.call(vis.xAxis);
       vis.yAxisGroup.call(vis.yAxis);
     }
@@ -104,36 +139,48 @@ class MagnitudeChart {
         const vis = this;
         vis.currentAttribute = newAttribute;
       
+        // Update x-axis label and chart title based on the attribute
         if (newAttribute === 'depth') {
-          // Clamp domain to ignore the top 1% outliers
+          vis.xLabel.text('Depth (km)');
+          vis.title.text('Histogram of Earthquakes by Depth');
+        } else if (newAttribute === 'duration') {
+          vis.xLabel.text('Duration');
+          vis.title.text('Histogram of Earthquakes by Duration');
+        } else {
+          // default to magnitude
+          vis.xLabel.text('Magnitude');
+          vis.title.text('Histogram of Earthquakes by Magnitude');
+        }
+      
+        // Then apply domain logic
+        if (newAttribute === 'depth') {
+          // Example: clamp domain to ignore top outliers
           const sortedDepths = vis.data.map(d => d.depth).sort(d3.ascending);
-          const cutoff = d3.quantile(sortedDepths, 0.99);
+          const cutoff = d3.quantile(sortedDepths, 0.99); // 99th percentile
           vis.xScale.domain([0, cutoff]).nice();
       
-          // Or just do a normal extent if you prefer:
-          // const extent = d3.extent(vis.data, d => d.depth);
-          // vis.xScale.domain(extent).nice();
-      
-          // Use more bins
+          // Use more bins if desired
           vis.histogram
             .value(d => d.depth)
             .domain(vis.xScale.domain())
             .thresholds(vis.xScale.ticks(30)); // 30 bins
         } else {
-          // default approach for magnitude, duration, etc.
+          // Default approach for magnitude (or other attributes)
           const attrExtent = d3.extent(vis.data, d => d[newAttribute]);
           vis.xScale.domain(attrExtent).nice();
+      
           vis.histogram
             .value(d => d[newAttribute])
             .domain(vis.xScale.domain())
             .thresholds(vis.xScale.ticks(vis.config.bins));
         }
       
-        // Update the x-axis
+        // Update the x-axis scale
         vis.xAxis.scale(vis.xScale);
+      
         // Redraw
         vis.updateVis();
-      }
+      }      
       
   }
   
--- js/timeline.js
+++ js/timeline.js
@@ -1,116 +1,143 @@
 class Timeline {
-  constructor(_config, _data) {
-    this.config = {
-      parentElement: _config.parentElement,
-      margin: { top: 20, right: 20, bottom: 30, left: 40 },
-      width: 500,
-      height: 300,
-      ..._config
-    };
-    this.data = _data;
-    this.initVis();
-  }
-
-  initVis() {
-    const vis = this;
-
-    vis.width = vis.config.width - vis.config.margin.left - vis.config.margin.right;
-    vis.height = vis.config.height - vis.config.margin.top - vis.config.margin.bottom;
-
-    // Create the SVG element within the timeline container
-    vis.svg = d3.select(vis.config.parentElement)
-      .append('svg')
-      .attr('width', vis.config.width)
-      .attr('height', vis.config.height)
-      .append('g')
-      .attr('transform', `translate(${vis.config.margin.left}, ${vis.config.margin.top})`);
-
-    // Define scales for time (x) and quake count (y)
-    vis.xScale = d3.scaleTime().range([0, vis.width]);
-    vis.yScale = d3.scaleLinear().range([vis.height, 0]);
-
-    // Define axes
-    vis.xAxis = d3.axisBottom(vis.xScale).ticks(5);
-    vis.yAxis = d3.axisLeft(vis.yScale).ticks(5);
-
-    // Create groups for the axes
-    vis.xAxisGroup = vis.svg.append('g')
-      .attr('class', 'x-axis')
-      .attr('transform', `translate(0, ${vis.height})`);
-
-    vis.yAxisGroup = vis.svg.append('g')
-      .attr('class', 'y-axis');
-
-    // Call updateVis to draw the initial line chart
-    vis.updateVis();
-  }
-
-  updateVis() {
-    const vis = this;
-
-    // 1) Group earthquakes by day using the parsed Date
-    const grouped = d3.rollups(
-      vis.data,
-      v => v.length,
-      d => +d3.timeDay.floor(new Date(d.time))
-    );
-
-    // Convert the grouped data into an array of objects with 'day' and 'count'
-    vis.aggregatedData = grouped.map(([dayNumeric, count]) => ({
-      day: new Date(dayNumeric),
-      count
-    }));
-
-    // Sort the data by day
-    vis.aggregatedData.sort((a, b) => d3.ascending(a.day, b.day));
-
-    // 2) Update scales based on the aggregated data
-    vis.xScale.domain(d3.extent(vis.aggregatedData, d => d.day));
-    vis.yScale.domain([0, d3.max(vis.aggregatedData, d => d.count)]);
-
-    // 3) Update axes with the new scales
-    vis.xAxisGroup.call(vis.xAxis);
-    vis.yAxisGroup.call(vis.yAxis);
-
-    // 4) Create a line generator for the timeline
-    const lineGenerator = d3.line()
-      .x(d => vis.xScale(d.day))
-      .y(d => vis.yScale(d.count));
-
-    // Bind the aggregated data to a path element for the line chart.
-    let path = vis.svg.selectAll('.line-chart').data([vis.aggregatedData]);
-    path = path.join('path')
-      .attr('class', 'line-chart')
-      .attr('fill', 'none')
-      .attr('stroke', 'steelblue')
-      .attr('stroke-width', 2)
-      .attr('d', lineGenerator);
-
-    // 5) Optionally, add circles at each data point for enhanced tooltips
-    let circles = vis.svg.selectAll('.data-point')
-      .data(vis.aggregatedData, d => d.day);
-    circles.join('circle')
-      .attr('class', 'data-point')
-      .attr('cx', d => vis.xScale(d.day))
-      .attr('cy', d => vis.yScale(d.count))
-      .attr('r', 3)
-      .attr('fill', 'steelblue')
-      .on('mouseover', (event, d) => {
-        d3.select('#tooltip')
-          .style('opacity', 1)
-          .html(`
+    constructor(_config, _data) {
+      this.config = {
+        parentElement: _config.parentElement,
+        margin: { top: 40, right: 30, bottom: 50, left: 60 },
+        ..._config
+      };
+      this.data = _data;
+  
+      // We'll define a 600x400 internal coordinate system
+      this.internalWidth = 600;
+      this.internalHeight = 400;
+  
+      this.initVis();
+    }
+  
+    initVis() {
+      const vis = this;
+  
+      // Create the responsive SVG
+      vis.svg = d3.select(vis.config.parentElement)
+        .append('svg')
+        .attr('viewBox', `0 0 ${vis.internalWidth} ${vis.internalHeight}`)
+        .attr('preserveAspectRatio', 'xMidYMid meet')
+        .classed('svg-content', true);
+  
+      // Chart area
+      vis.width = vis.internalWidth - vis.config.margin.left - vis.config.margin.right;
+      vis.height = vis.internalHeight - vis.config.margin.top - vis.config.margin.bottom;
+  
+      vis.chartArea = vis.svg.append('g')
+        .attr('transform', `translate(${vis.config.margin.left}, ${vis.config.margin.top})`);
+  
+      // Define scales
+      vis.xScale = d3.scaleTime().range([0, vis.width]);
+      vis.yScale = d3.scaleLinear().range([vis.height, 0]);
+  
+      // Define axes
+      vis.xAxis = d3.axisBottom(vis.xScale).ticks(5);
+      vis.yAxis = d3.axisLeft(vis.yScale).ticks(5);
+  
+      // Axis groups
+      vis.xAxisGroup = vis.chartArea.append('g')
+        .attr('class', 'x-axis')
+        .attr('transform', `translate(0, ${vis.height})`);
+  
+      vis.yAxisGroup = vis.chartArea.append('g')
+        .attr('class', 'y-axis');
+  
+      // Labels
+      vis.xAxisLabel = vis.chartArea.append('text')
+        .attr('x', vis.width / 2)
+        .attr('y', vis.height + 35)
+        .style('text-anchor', 'middle')
+        .text('Date');
+  
+      vis.yAxisLabel = vis.chartArea.append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('x', -vis.height / 2)
+        .attr('y', -35)
+        .style('text-anchor', 'middle')
+        .text('Magnitude');
+  
+      // Title
+      vis.title = vis.chartArea.append('text')
+        .attr('x', vis.width / 2)
+        .attr('y', -10)
+        .style('text-anchor', 'middle')
+        .style('font-size', '16px')
+        .text('Earthquakes Over Time');
+  
+      vis.updateVis();
+    }
+  
+    updateVis() {
+      const vis = this;
+  
+      // Example: group by day
+      const grouped = d3.rollups(
+        vis.data,
+        v => v.length,
+        d => +d3.timeDay.floor(new Date(d.time))
+      );
+  
+      vis.aggregatedData = grouped.map(([dayNumeric, count]) => ({
+        day: new Date(dayNumeric),
+        count
+      }));
+      vis.aggregatedData.sort((a, b) => d3.ascending(a.day, b.day));
+  
+      // Scales
+      vis.xScale.domain(d3.extent(vis.aggregatedData, d => d.day));
+      vis.yScale.domain([0, d3.max(vis.aggregatedData, d => d.count)]);
+  
+      // Axes
+      vis.xAxisGroup.call(vis.xAxis);
+      vis.yAxisGroup.call(vis.yAxis);
+  
+      // Line generator
+      const lineGenerator = d3.line()
+        .x(d => vis.xScale(d.day))
+        .y(d => vis.yScale(d.count));
+  
+      // Draw the path
+      let path = vis.chartArea.selectAll('.line-chart')
+        .data([vis.aggregatedData]);
+      path.join('path')
+        .attr('class', 'line-chart')
+        .attr('fill', 'none')
+        .attr('stroke', 'steelblue')
+        .attr('stroke-width', 2)
+        .attr('d', lineGenerator);
+  
+      // Circles for tooltip
+      let circles = vis.chartArea.selectAll('.data-point')
+        .data(vis.aggregatedData, d => d.day);
+  
+      circles.join('circle')
+        .attr('class', 'data-point')
+        .attr('cx', d => vis.xScale(d.day))
+        .attr('cy', d => vis.yScale(d.count))
+        .attr('r', 3)
+        .attr('fill', 'steelblue')
+        .on('mouseover', (event, d) => {
+          d3.select('#tooltip')
+            .style('opacity', 1)
+            .html(`
               <b>Date:</b> ${d.day.toDateString()}<br>
               <b>Quakes:</b> ${d.count}
             `);
-      })
-      .on('mousemove', (event) => {
-        d3.select('#tooltip')
-          .style('left', (event.pageX + 10) + 'px')
-          .style('top', (event.pageY + 10) + 'px');
-      })
-      .on('mouseleave', () => {
-        d3.select('#tooltip')
-          .style('opacity', 0);
-      });
+        })
+        .on('mousemove', (event) => {
+          d3.select('#tooltip')
+            .style('left', (event.pageX + 10) + 'px')
+            .style('top', (event.pageY + 10) + 'px');
+        })
+        .on('mouseleave', () => {
+          d3.select('#tooltip')
+            .style('opacity', 0);
+        });
+    }
   }
-}
+  
